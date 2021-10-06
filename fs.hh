38f591049547ecaa5f2b525f925208bcbccff5a7
MESYETI <52859743+MESYETI@users.noreply.github.com>
2021-10-06 17:10:55 +0100

    Delete fs.hh

--- fs.hh
+++
@@ -1,8 +0,0 @@
-#include <string>
-using std::string;
-bool pexists(string pname);
-void pcreate(string pname);
-bool fexists(string fname);
-bool o_fexists(string fname);
-string fread(string fname);
-void fcreate(string fname);
